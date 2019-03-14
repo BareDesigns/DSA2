235ba2ef4d59352ad5bd3cb3754b6b033993cf7d
Jonathan Nguyen <jon.p.nguyen@pm.me>
2019-03-13 21:26:34 -0500

    Got variables set

--- DSA_2.py
+++ DSA_2.py
@@ -1,8 +1,8 @@
 # Jonathan Nguyen; #000918228
 
 import json
-import pandas
-from string import ascii_uppercase
+from string import ascii_lowercase
+# from vertex_data import *
 
 
 class Vertex:
@@ -28,35 +28,32 @@ class Graph:
         self.adjacency_list[from_vertex].append(to_vertex)
 
     def add_undir_edge(self, vertex_a, vertex_b, weight=1.0):
-        # Function to add an undirected edge and the weight of that edge
         self.add_dir_edge(vertex_a, vertex_b, weight)
-        self.add_undir_edge(vertex_b, vertex_a, weight)
+        self.add_dir_edge(vertex_b, vertex_a, weight)
 
 
-def master_list():
-    '''This function puts the location names and vertex names into 1 dictionary'''
-
-    with open('Distance_Table.json') as data_file:
-        places = json.load(data_file)
-        for b in places['locations']:
-            location_names.append(b)
-    for letter in ascii_uppercase:
-        vertex_names.append('vertex_' + letter)
-    location_dict1 = dict(zip((vertex_names), (location_names)))
-    return location_dict1
-
-
-g = Graph()
-vertex_HUB = Vertex("WGU")
-# Creating list variables for merging with function
 location_names = []
 vertex_names = []
-location_dict1 = {}
+location_dict = {}
 
+with open('Distance_Table.json') as data_file:
+    places = json.load(data_file)
+    for b in places['locations']:
+        location_names.append(b)
+for letter in ascii_lowercase:
+    vertex_names.append('vertex_' + letter)
+location_dict = dict(zip((vertex_names), (location_names)))
+
+g = Graph()
 
 # Assign the graph variables to the location names
-for v, x in location_dict1.items():
-    v = Vertex(str(x))
+for k, v in location_dict.items():
+    globals()[k] = Vertex(v)
 
-print(master_list())
-print(Graph)
+
+vertex_hub = Vertex("WGU")
+g.add_vertex(vertex_hub)
+
+g.add_undir_edge(vertex_hub, vertex_t, 1.2)
+g.add_undir_edge(vertex_HUB, vertex_v, 2.4)
+g.add_undir_edge(vertex_HUB, vertex_q, 2.0)
